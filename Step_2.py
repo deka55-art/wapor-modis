011fbe364664da007d8de06244a6a30dc4cde939
deka55-art <kaan.err@hotmail.com>
2025-04-29 10:02:55 +0300

    Update Step_2.py

--- Step_2.py
+++ Step_2.py
@@ -7,26 +7,21 @@ folder_path = "C:/Users/Casper/Desktop/Kişisel/Kaan/Article/Shapefiles/BE_Lon_M
 tif_files = sorted(glob.glob(os.path.join(folder_path, "*.tif")))
 
 
-# 2. Tüm .tif dosyalarının listesini al
 tif_files = sorted(glob.glob(os.path.join(folder_path, "*.tif")))
 
-# 3. Her tif dosyasını aç, 1000 ile çarp, yeni dosya olarak kaydet
 for tif_file in tif_files:
     with rasterio.open(tif_file) as src:
         data = src.read(1).astype(float)
         meta = src.meta.copy()
         
-        # NoData'yı koru
         data[data == src.nodata] = np.nan
         
-        # ✅ kgC → gC çevirisi: 1000 ile çarp
+        # Convert kgC to gC and apply scale factor
         data_converted = data * 0.1
 
-    # Yeni dosya ismi oluştur
     filename = os.path.basename(tif_file)
     output_path = os.path.join(folder_path, f"converted_{filename}")
     
-    # GeoTIFF olarak kaydet
     meta.update(dtype=rasterio.float32)
 
     with rasterio.open(output_path, 'w', **meta) as dst:
